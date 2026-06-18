1e347d84c6d1c078acbed9bd1730f33519b3336a
GL1TCH <ShadowVR7913@gmail.com>
2026-06-18 16:11:12 -0400

    Change logger name from 'spotify-api' to 'Glitch@SPI'

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -4,5 +4,5 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
-logger = logging.getLogger("spotify-api")
+logger = logging.getLogger("Glitch@SPI")
 
